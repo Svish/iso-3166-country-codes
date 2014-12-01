169eff94186bd03dc89350cf94a96c5f3d7bc3ab
Torleif Berger <torleif@outlook.com>
2014-12-01 11:00:08 +0000

    Updates from 2014-11-18 change notification - Found only updates to sub-divisions and sources, so no changes done

--- countries.sql
+++ countries.sql
@@ -1,7 +1,7 @@
 /**
   * Country names, continent names and ISO-3166 codes.
   *
-  * @updated  2014-04-18
+  * @updated  2014-12-01
   * @link     http://www.geekality.net/?p=1182
   * @link     https://bitbucket.org/svish/iso-3166-country-codes
   *
