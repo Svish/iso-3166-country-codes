6707abdd2939860412b1b764fca4fc92df59c0be
Torleif Berger <torleif@outlook.com>
2020-02-21 20:10:44 +0100

    Changed to CHAR for number column
    Fixes #1

--- countries.sql
+++ countries.sql
@@ -27,7 +27,7 @@ CREATE TABLE `countries` (
   `name` VARCHAR(255) NOT NULL COMMENT 'English country name',
   `full_name` VARCHAR(255) NOT NULL COMMENT 'Full English country name',
   `iso3` CHAR(3) NOT NULL COMMENT 'Three-letter country code (ISO 3166-1 alpha-3)',
-  `number` SMALLINT(3) ZEROFILL NOT NULL COMMENT 'Three-digit country number (ISO 3166-1 numeric)',
+  `number` CHAR(3) NOT NULL COMMENT 'Three-digit country number (ISO 3166-1 numeric)',
   `continent_code` CHAR(2) NOT NULL,
   PRIMARY KEY (`code`),
   KEY `continent_code` (`continent_code`),
